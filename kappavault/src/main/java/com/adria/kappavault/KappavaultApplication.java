9e8e1b097e9d92d804eca466d52b307c0883ade8
DjAdria001 <adria30042004@gmail.com>
2026-05-19 08:37:59 +0200

    Initial commit

---
+++ kappavault/src/main/java/com/adria/kappavault/KappavaultApplication.java
@@ -0,0 +1,13 @@
+package com.adria.kappavault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KappavaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KappavaultApplication.class, args);
+	}
+
+}
